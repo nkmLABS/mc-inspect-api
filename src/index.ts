d35ca7debb6280dd5048dda4df31c6a22fc775e3
Niklas Maroldt <nik.dev@gmx.net>
2026-04-06 06:58:00 +0200

    remove authentication checker

--- src/index.ts
+++ src/index.ts
@@ -2,24 +2,11 @@ import { createResponse } from './shared/response';
 import { handlePlayer } from './api/players/handler';
 import { handleServer } from './api/servers/handler';
 
-// Allowed origins whitelist
-const allowedProductionOrigins: string[] = ['https://mc-inspect.pages.dev', 'https://mc-inspect.buildbynik.dev'];
-const allowedLocalOrigins: string[] = ['http://localhost:3000', 'http://127.0.0.1:3000', 'http://localhost:3001', 'http://127.0.0.1:3001'];
-
 // Main handler and router
 export default {
   async fetch(req, env, ctx): Promise<Response> {
-    const origin = req.headers.get('Origin') || '';
-    const apiKey = req.headers.get('X-API-Key') || '';
-
-    const isProductionOrigin = allowedProductionOrigins.includes(origin);
-    const isLocalOrigin = allowedLocalOrigins.includes(origin);
-
     // Handle preflight request
     if (req.method === 'OPTIONS') return createResponse({}, origin, 200, { 'Access-Control-Max-Age': '86400' });
-
-    // Handle forbidden request (not production origin and not local origin with correct key)
-    if (!isProductionOrigin && !(isLocalOrigin && apiKey === env.API_KEY)) return createResponse({ error: 'Forbidden' }, origin, 403);
 
     // Handle wrong request method
     if (req.method !== 'GET') return createResponse({ error: 'Method Not Allowed' }, origin, 405);
